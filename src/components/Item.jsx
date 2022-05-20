78795cb5285ef0a1e1904535421724935d0c6b63
AndresJoaquinNino <andresninorodil@gmail.com>
2022-05-20 15:40:19 -0400

    Display more info

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,10 +1,11 @@
-import { Stack, ListItem, ListItemText, IconButton } from "@mui/material"
+import { Stack, ListItem, ListItemText, IconButton, Typography } from "@mui/material"
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import { useDispatch } from "react-redux";
 
 const Item = ({ dataItem }) => {
-    const { name, id } = dataItem
+    const { id, name, currency, price } = dataItem
+
     const dispatch = useDispatch()
 
     const deleteItem = () => {
@@ -23,7 +24,14 @@ const Item = ({ dataItem }) => {
                 </IconButton>
             </Stack>
         }>
-            <ListItemText primary={name} />
+            <ListItemText  sx={{color: '#000'}}>
+                <Typography variant="h6" component="span">
+                    { name } - { price }
+                </Typography>
+                <Typography component="span">
+                    { currency }
+                </Typography>
+            </ListItemText>
         </ListItem>
     )
 }
