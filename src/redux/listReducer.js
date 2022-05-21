91f8d76af695ae5706c7e8494b18e3abad979ca9
AndresJoaquinNino <andresninorodil@gmail.com>
2022-05-21 12:14:54 -0400

    Creating listReducer

---
+++ src/redux/listReducer.js
@@ -0,0 +1,31 @@
+import { setLocalStorage, createUuid } from "../helpers"
+
+
+const initialState = []
+
+const listReducer = ( state = initialState, action ) => {
+    switch(action.type){
+        case 'list/add' : {
+            const newItem = { ...action.payload, id : createUuid() }
+            const newListItems = [ ...state, newItem ]
+            setLocalStorage('listItems',newListItems)
+            return newListItems
+        }
+        case 'list/delete' : {
+            const itemId = action.payload
+            const indexItem = state.findIndex((ele) => ele.id === itemId)
+            state.splice(indexItem, 1)
+            setLocalStorage('listItems',[...state])
+            return [...state]
+        }
+        case 'list/edit' : {
+            const { id:itemId } = action.payload
+            const newListItem = state.map((ele) => ele.id === itemId ? action.payload :  ele)
+            return newListItem
+        }
+        default:
+            return state
+    }
+}
+
+export default listReducer
