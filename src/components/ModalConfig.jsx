6cd739cb22f5ffb564bd01aaf11f7f2693b112a2
AndresJoaquinNino <andresninorodil@gmail.com>
2022-05-26 13:22:48 -0400

    Creating ModalConfig

---
+++ src/components/ModalConfig.jsx
@@ -0,0 +1,53 @@
+import { Modal, Box, Typography, TextField, InputAdornment, Stack, Button } from "@mui/material"
+import SaveIcon from '@mui/icons-material/Save';
+import { grey } from "@mui/material/colors";
+
+const ModalConfig = () => {
+
+    const handleClose = () => {}
+
+    return(
+        <Modal open={true} onClose={handleClose} sx={{padding:2,display:'grid',placeItems:'center'}}>
+            <Box className="box-modal">
+                <Typography variant="h4" component="h4" fontWeight='bold' align="center" marginBottom={3}>
+                    Configs Setting
+                </Typography>
+
+                <Stack direction='row' spacing={2} justifyContent='center'>
+                    <TextField
+                    variant="standard"
+                    defaultValue={'1'}
+                    InputProps={{
+                        readOnly : true,
+                        endAdornment: (
+                            <InputAdornment position="start">
+                                USD
+                            </InputAdornment>
+                        ),
+                    }}
+                    sx={{marginBottom: 4, width: '4em'}}
+                    />
+                    <Typography variant="h6" component="span" fontWeight='bold' color={grey[600]}>
+                        =
+                    </Typography>
+                    <TextField
+                    variant="standard"hhg
+                    InputProps={{
+                        endAdornment: (
+                            <InputAdornment position="start">
+                                USD
+                            </InputAdornment>
+                        ),
+                    }}
+                    sx={{marginBottom: 4, width: '6em'}}
+                    />
+                </Stack>
+                <Button type="submit" variant="contained" sx={{width:'50%'}} startIcon={ <SaveIcon/> }>
+                    Save Config
+                </Button>
+            </Box>
+        </Modal>
+    )
+}
+
+export default ModalConfig
