14f67274edbb5aee755350b46fa07a80811428fc
AndresJoaquinNino <andresninorodil@gmail.com>
2022-05-27 08:37:51 -0400

    Creating a search bar

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,4 +1,5 @@
-import { Divider ,Box, Stack, Typography, Button, List } from "@mui/material"
+import { Divider ,Box, Stack, Typography, Button, List, TextField, InputAdornment } from "@mui/material"
+import SearchIcon from "@mui/icons-material/Search";
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import SettingsIcon from '@mui/icons-material/Settings';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
@@ -7,12 +8,22 @@ import ModalEditItem from "../components/ModalEditItem";
 import ModalConfig from "../components/ModalConfig";
 import Item from "../components/Item";
 import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
 
 const Main = () => {
 
+    const [searchValue, setSearchValue] = useState('')
+    const handleChange = ({ target }) => setSearchValue(target.value)
+
     const dispatch = useDispatch()
 
-    const listItems = useSelector((state) => state.listItems)
+    const listItems = useSelector(({ listItems }) => {
+        const result = listItems.filter((ele) => {
+            const searchedElement = ele.name.substring(0,searchValue.length).toLowerCase()
+            return searchValue === searchedElement
+        })
+        return result
+    })
 
     const [totalVES, totalUSD] = useSelector((state) => {
         const { listItems, config } = state
@@ -48,6 +59,20 @@ const Main = () => {
                         New Item
                     </Button>
                 </Stack>
+                <TextField
+                    variant="standard"
+                    placeholder="Search"
+                    InputProps={{
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <SearchIcon/>
+                            </InputAdornment>
+                        ),
+                    }}
+                    value={searchValue}
+                    onChange={handleChange}
+                    sx={{marginBottom:3, width: '100%'}}/>
+
                 <List sx={{maxHeight : '10rem',overflow: 'auto',marginBottom: 1}}>
                     {
                         listItems.map((ele,index) => (
